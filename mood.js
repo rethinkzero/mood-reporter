551d4da2e7805e4ebb0018052216667f885d89f7
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-05-02 13:12:40 -0400

    Create mood.js

---
+++ mood.js
@@ -0,0 +1,43 @@
+// Mood Reporter
+// Your Name
+// Date
+
+let userName = "Taylor";
+let moodToday = "happy";
+
+//
+
+function respondToMood(name, mood) {
+  console.log("Hi " + name + "!");
+
+  if (mood === "happy") {
+    console.log("That’s awesome. Keep it going!");
+  } else if (mood === "tired") {
+    console.log("Don’t forget to rest. You’ve earned it.");
+  } else if (mood === "exhausted") {
+    console.log("Take a breath. Better days are ahead.");
+  } else {
+    console.log("Thanks for checking in. Keep being you.");
+  }
+}
+
+respondToMood(userName, moodToday);
+
+for (let i = 0; i < 3; i++) {
+  console.log("You've got this, " + name + "!");
+}
+
+function getMoodMessage(name, mood) {
+  let message = "Hey " + name+ "! ";
+
+  if (mood === "happy") {
+    message += "Keep smiling!";
+  } else {
+    message += "You're doing your best. That matters.";
+  }
+
+  return message;
+}
+
+let finalMessage = getMoodMessage("Jordan", "stressed");
+console.log(message);
